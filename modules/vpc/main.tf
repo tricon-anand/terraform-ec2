483687d6f544fc85b264aa9930a1e58464f86dd9
tricon-anand <anand@triconinfotech.com>
2023-11-27 23:59:45 +0530

    updating route tablevalue

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -45,7 +45,7 @@ resource "aws_subnet" "private-subnet" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
