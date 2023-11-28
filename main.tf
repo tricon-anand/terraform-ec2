8210dcc56eba1e182f154b800bd9e928c9bfe381
tricon-anand <anand@triconinfotech.com>
2023-11-28 16:15:12 +0530

    create key pair

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  instance_type = "t2.micro"
-  ami="ami-0fc5d935ebf8bc3bc"
-  instance_names = "dev"
-  aws_keypair = module.keyPairSSH.aws_keypair
-}
+# module "ec2_instance" {
+#   source = "./modules/ec2_instance"
+#   instance_type = "t2.micro"
+#   ami="ami-0fc5d935ebf8bc3bc"
+#   instance_names = "dev"
+#   aws_keypair = module.keyPairSSH.aws_keypair
+# }
 
 module "s3_bucket" {
   source = "./modules/s3_bucket"
--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -29,7 +29,6 @@ resource "aws_security_group" "TF_SG" {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -38,7 +37,6 @@ resource "aws_security_group" "TF_SG" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -47,7 +45,6 @@ resource "aws_security_group" "TF_SG" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -55,10 +52,19 @@ resource "aws_security_group" "TF_SG" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
     Name = "test"
+  }
+}
+
+
+resource "aws_key_pair" "demoKeyPair" {
+  key_name = "demoKeyPair"
+  public_key = tls_private_key.rsapublickey.private_key_openssh
+
+  tags = {
+    Name = "demoKeyPair"
   }
 }
